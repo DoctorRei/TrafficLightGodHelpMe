9c046203f6a79dce5f21dd3a4091f4ff3a9d09b2
AkiraRei <Akirarei@yandex.ru>
2023-05-21 14:14:21 +0300

    Final ver. Added swap .title for button

--- TrafficLightGodHelpMe/ViewController.swift
+++ TrafficLightGodHelpMe/ViewController.swift
@@ -31,6 +31,8 @@ class ViewController: UIViewController {
     }
 
     @IBAction func pushNextColor() {
+        startButton.setTitle("Next!", for: .normal)
+        
         switch countTaps {
         case 0:
             trafficYelow.alpha = 0.3
